d6aa554eb3c67e5a59741c536446e25955ad1984
nosredna1981 <nosredna1981@gmail.com>
2024-11-05 21:45:45 -0300

    validator password

--- js/validator.js
+++ js/validator.js
@@ -2,6 +2,7 @@ validateName()
 validateRequired()
 validateEmail()
 validateTel()
+validatePassword()
 
 function validateName() {
     const element = document.querySelector("#nameInput")
@@ -10,7 +11,7 @@ function validateName() {
         const value = e.target.value
         console.log(value)
         let content = ''
-        if (value == '') {          
+        if (value == '') {
             content = '<span class="text-danger"> Nome inválido </span>'
             // message.innerHTML = '<span class="text-danger"> Nome invalido </span>'
         }
@@ -29,7 +30,7 @@ function validateEmail() {
         console.log(value)
         let error = ''
         // if (value.indexOF('@') === -1 || value.indexOF('.') === -1) {
-            const regexEmail = (/^[a-z\d]+\@[a-z]+\.([a-z]{3})+$/g)
+        const regexEmail = (/^[a-z\d]+\@[a-z]+\.([a-z]{3})+$/g)
         if (!regexEmail.test(value)) {
             error = "E-mail inválido"
         }
@@ -45,11 +46,24 @@ function validateTel() {
         const value = e.target.value
         const regexTel = /^\([0-9]{2,3}\)\s[0-9]{5}-[0-9]{4}$/g
         let error = ''
-        if (!regexTel.test(value)) {          
+        if (!regexTel.test(value)) {
             error = 'Telefone inválido. Formato: "(999) 99999-9999"'
-        }    
+        }
         message.innerHTML = error
         // SetContentElement("#telHelp", error)        
+    })
+}
+
+function validatePassword() {
+    const element = document.querySelector("#passwordInput")
+    const message = document.querySelector("#passwordHelp")
+    element.addEventListener('keyup', (e) => {
+        const value = e.target.value
+        if (value.lenght < 8) {
+            menssage.innerHTML = "Senha Muito curta! Minimo de 8 Caracteres"
+        }else {
+            message.innerHTML = ''
+        }
     })
 }
 

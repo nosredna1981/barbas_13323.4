890211ccb101b327b993e14c21bad26deb420adc
nosredna1981 <nosredna1981@gmail.com>
2024-11-14 21:37:07 -0300

    mock

--- components/card-component/card-component.js
+++ components/card-component/card-component.js
@@ -1,40 +1,61 @@
-export function cardcomponent() {
+
+
+
+export async function cardcomponent() {
     const localTemplate = 'components/card-component/card-component.html'
     const localStyle = 'components/card-component/card-component.css'
     const element = document.getElementById('card-component')
     if (!element) return
-
-
-    //Style -> css
-    element.innerHTML = ''
-    element.innerHTML += `<link rel="stylesheet" href="${localStyle}">`
-    
-    //Dados
-    const services = getServices()
-    console.log('>>> services ' , services);
-
-
-    //Template -> HTML
+    //Pega Dados
+    const services = await getServices()
+    console.log('>>> services ', services);
+    // Pega Template
     fetch(localTemplate)
         .then((res) => res.text())
         .then((component) => {
-            element.innerHTML += component
+            //Template -> HTML
+            element.innerHTML = mountService(services, component)
+
+            //Style -> css   
+            element.innerHTML += `<link rel="stylesheet" href="${localStyle}">`
+
+        })
+        .catch((error) => {
+            console.error("Erro ao montar o component: ", error);
+        })
+}
+
+function mountService(dados, template) {
+    let result = ""
+    // for (let i = 0; i < dados.length; i++) {
+    //     let newTemplate = template
+    //     result += newTemplate
+    //         .replace('{{fotos}}', dados[i].fotos)
+    //         .replace('{{nome}}', dados[i].nome)
+    //         .replace('{{descricao}}', dados[i].descricao)
+    // }
+
+    for (const dado of dados) {
+        let newTemplate = template
+        result += newTemplate
+            .replace('{{fotos}}', dado.fotos)
+            .replace('{{nome}}', dado.nome)
+            .replace('{{descricao}}', dado.descricao)
+    }
+
+    return result
+}
+
+async function getServices() {
+    let result = []
+    await fetch('mock/service.json')
+        .then((res) => res.json())
+        .then((data) => {
+            result = data
         })
         .catch((error) => {
             console.error("Erro ao montar a navbar:", error);
         })
-}
-
-async function getServices() {
-let result = []
-    await fetch('mock/service.json')
-    .then((res) => res.json())
-    .then((data) => {
-        result = data
-    })
-    .catch((error) => {
-        console.error("Erro ao montar a navbar:", error);
-    })
     return result
 
 }
